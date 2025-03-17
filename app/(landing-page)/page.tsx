c49e04b27430e2f354cd53e777159a43187d8084
LianggaRistiana <liangga4@gmail.com>
2025-03-18 01:53:47 +0800

    feat: add dark mode button

--- app/(landing-page)/page.tsx
+++ app/(landing-page)/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import HeroTitle from "@/components/atoms/HeroTitle";
+import { ModeToggle } from "@/components/atoms/ModeToggle";
 import { useEffect, useState } from "react";
 
 export default function Home() {
@@ -24,6 +25,7 @@ export default function Home() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-items-center gap-y-2 md:gap-6 px-4">
       <HeroTitle title="Mapion" className="text-6xl text-center md:text-left" />
+      <ModeToggle></ModeToggle>
     </div>
   );
 }

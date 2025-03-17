e58e7ef125df8624cc3d4c54e0494f9b17d19889
LianggaRistiana <liangga4@gmail.com>
2025-03-18 01:42:22 +0800

    feat: add dark mode configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,29 @@
+"use client";
+
 import HeroTitle from "@/components/atoms/HeroTitle";
-import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    // Set initial value
+    setIsDarkMode(mediaQuery.matches);
+
+    // Listen for changes
+    const handleChange = (event: MediaQueryListEvent) => {
+      setIsDarkMode(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleChange);
+    console.log(isDarkMode);
+  }, [isDarkMode]);
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
-      <HeroTitle title="Mapion" className="text-6xl text-center" />
+    <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-items-center gap-y-2 md:gap-6 px-4">
+      <HeroTitle title="Mapion" className="text-6xl text-center md:text-left" />
     </div>
   );
 }
